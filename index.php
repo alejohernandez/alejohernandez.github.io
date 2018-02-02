d98c18bb776c85556fd1a31e464730f28382ebd7
Alejandro Hernandez <alejo170401@gmail.com>
2018-02-02 18:21:25 -0400

    prueba

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/style.css">
+	<title>Alejo Git Hub</title>
+</head>
+<body>
+	<h1>Holaa, esta es mi prueba!</h1>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum consequuntur, quibusdam quia eius at voluptas mollitia vero. Pariatur iusto quas delectus corporis, porro repudiandae neque voluptatibus assumenda tenetur consectetur. Fugit, reiciendis quam excepturi, natus voluptas ipsa vel veritatis ducimus, quasi consequatur molestias optio, iure quos. Nulla, repudiandae in voluptates quia!
+	</p>
+</body>
+</html>
